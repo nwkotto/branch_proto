1aa11a7117feffaa3d1f48e4829d2da27b77142b
Nick Otto <nickotto@farmlogs.com>
2015-10-11 15:50:52 -0400

    Added basic react components

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,164 +1,210 @@
 $(document).ready(function() {
-	var Grid = function() {
-		// Indexing in - data[row][column]
-		var priv = {
-			grid: [[true]],
 
-			// Expand the grid
-			addRow: function() {
-				var freshRow = this.grid[0].map(function(item) {
-					return false;
-				});
-				this.grid.push(freshRow);
-				return this;
-			},
-			addRows: function(number) {
-				return this._repeat("addRow", number);
-			},
-			addCol: function() {
-				this.grid = this.grid.map(function(item) {
-					item.push(false);
-					return item;
-				});
-				return this;
-			},
-			addCols: function(number) {
-				return this._repeat("addCol", number);
-			},
+    // ***** MODELS ***** //
+    var Grid = function() {
+        // Indexing in - data[row][column]
+        var priv = {
+            grid: [[true]],
 
-			// Internal methods
-			_repeat: function(method, number) {
-				for (var i = 0; i < number; ++i) {
-					this[method]();
-				}
-				return this;
-			}
-		}
+            // Expand the grid
+            addRow: function() {
+                var freshRow = this.grid[0].map(function(item) {
+                    return false;
+                });
+                this.grid.push(freshRow);
+                return this;
+            },
+            addRows: function(number) {
+                return this._repeat("addRow", number);
+            },
+            addCol: function() {
+                this.grid = this.grid.map(function(item) {
+                    item.push(false);
+                    return item;
+                });
+                return this;
+            },
+            addCols: function(number) {
+                return this._repeat("addCol", number);
+            },
 
-		var pub = {
-			// Getters and setters
-			getBlock: function(row, col) {
-				if (row < priv.grid.length && col < priv.grid[0].length) {
-					return priv.grid[row][col];
-				}
-				return null;
-			},
-			setBlock: function(row, col, blockData) {
-				priv.addRows((row + 1) - priv.grid.length).addCols((col + 1) - priv.grid[0].length);
-				console.log(priv.grid);
-				priv.grid[row][col] = blockData;
-			},
+            // Internal methods
+            _repeat: function(method, number) {
+                for (var i = 0; i < number; ++i) {
+                    this[method]();
+                }
+                return this;
+            }
+        }
 
-			// Metadata methods
-			numRows: function() {
-				return priv.grid.length;
-			},
-			numCols: function() {
-				return priv.grid[0].length;
-			}
-		}
+        var pub = {
+            // Getters and setters
+            getBlock: function(row, col) {
+                if (row < priv.grid.length && col < priv.grid[0].length) {
+                    return priv.grid[row][col];
+                }
+                return null;
+            },
+            setBlock: function(row, col, blockData) {
+                priv.addRows((row + 1) - priv.grid.length).addCols((col + 1) - priv.grid[0].length);
+                console.log(priv.grid);
+                priv.grid[row][col] = blockData;
+            },
 
-		return pub;
-	}
+            // Metadata methods
+            numRows: function() {
+                return priv.grid.length;
+            },
+            numCols: function() {
+                return priv.grid[0].length;
+            }
+        }
 
-	var Board = function(params) {
-		var params = params || {};
-		var priv = {
-			grid: new Grid(),
-			$object: $(params.selector || ".board"),
-			blockSpec: {
-				col: parseInt(params.width, 10) || 300,
-				row: parseInt(params.height, 10) || 185
-			},
-			updateBounds: function() {
-				// Specify board params based on grid size
-				var width = this.blockSpec.col * this.grid.numCols(),
-					height = this.blockSpec.row * this.grid.numRows();
+        return pub;
+    }
 
-				// Resize board; re-specify bounds
-				var $board = this.$object;
-				$board.css({
-					"width": width + "px",
-					"height": height + "px"
-				});
-				this.bounds = {
-					"col": {
-						"start": $board.position().left,
-						"end": $board.position().left + $board.width()
-					},
-					"row": {
-						"start": $board.position().top,
-						"end": $board.position().top + $board.height()
-					}
-				}
-				return this;
-			},
-			rowAndCol: function(coords) {
-				var indexes = {
-					"row": -1,
-					"col": -1
-				};
+    var Board = function(params) {
+        var params = params || {};
+        var priv = {
+            grid: new Grid(),
+            $object: $(params.selector || ".board"),
+            blockSpec: {
+                col: parseInt(params.width, 10) || 300,
+                row: parseInt(params.height, 10) || 185
+            },
+            updateBounds: function() {
+                // Specify board params based on grid size
+                var width = this.blockSpec.col * this.grid.numCols(),
+                    height = this.blockSpec.row * this.grid.numRows();
 
-				// Check row and col bounds
-				for (var dimension in this.bounds) {
-					console.log(coords[dimension]);
-					if (coords[dimension] > this.bounds[dimension].start && coords[dimension] < this.bounds[dimension].end) {
-						var difference = coords[dimension] - this.bounds[dimension].start;
-						indexes[dimension] = Math.floor(difference / this.blockSpec[dimension]);
-					}
-				}
+                // Resize board; re-specify bounds
+                var $board = this.$object;
+                $board.css({
+                    "width": width + "px",
+                    "height": height + "px"
+                });
+                this.bounds = {
+                    "col": {
+                        "start": $board.position().left,
+                        "end": $board.position().left + $board.width()
+                    },
+                    "row": {
+                        "start": $board.position().top,
+                        "end": $board.position().top + $board.height()
+                    }
+                }
+                return this;
+            },
+            rowAndCol: function(coords) {
+                var indexes = {
+                    "row": -1,
+                    "col": -1
+                };
 
-				return indexes;
-			},
-			createBlockObject: function(row, col) {
-				var styles = "left:" + (col * this.blockSpec.col).toString() + "px; top:" + (row * this.blockSpec.row).toString() + "px;";
-				this.$object.append("<div class='block' style='" + styles + "'></div>");
-			}
-		};
+                // Check row and col bounds
+                for (var dimension in this.bounds) {
+                    console.log(coords[dimension]);
+                    if (coords[dimension] > this.bounds[dimension].start && coords[dimension] < this.bounds[dimension].end) {
+                        var difference = coords[dimension] - this.bounds[dimension].start;
+                        indexes[dimension] = Math.floor(difference / this.blockSpec[dimension]);
+                    }
+                }
 
-		var pub = {
-			addBlock: function(row, col) {
-				priv.grid.setBlock(row, col, {});
-				priv.updateBounds();
-				priv.createBlockObject(row, col);
-			},
-			nearestOpenBlock: function(y, x) {
-				var rowAndCol = priv.rowAndCol({
-					"row": y,
-					"col": x
-				});
-				console.log(priv.grid.getBlock(y, x));
-				return rowAndCol;
-			}
-		}
+                return indexes;
+            },
+            createBlockObject: function(row, col) {
+                var styles = "left:" + (col * this.blockSpec.col).toString() + "px; top:" + (row * this.blockSpec.row).toString() + "px;";
+                this.$object.append("<div class='block' style='" + styles + "'></div>");
+            }
+        };
 
-		// Initialization
-		priv.updateBounds();
+        var pub = {
+            addBlock: function(row, col) {
+                priv.grid.setBlock(row, col, {});
+                priv.updateBounds();
+                priv.createBlockObject(row, col);
+            },
+            nearestOpenBlock: function(y, x) {
+                var rowAndCol = priv.rowAndCol({
+                    "row": y,
+                    "col": x
+                });
+                console.log(priv.grid.getBlock(y, x));
+                return rowAndCol;
+            }
+        }
 
-		return pub;
-	}
+        // Initialization
+        priv.updateBounds();
 
-	var board = new Board();
+        return pub;
+    }
 
+    // ***** VIEWS ***** //
+    var BlockView = React.createClass({
+        getInitialState: function() {
 
-	var setBodySize = function() {
-		$("body").css({
-			"width": $(window).width() + "px",
-			"height": $(window).height() + "px"
-		});
-	}
-	$(window).resize(function() {
-		setBodySize();
-	});
+        },
+        render: function() {
+            return (
+                <div style={this.state.styles}>
+                    Allo
+                </div>
+            )
+        }
+    });
 
-	$(window).on("mousemove", function(e) {
-		console.log(board.nearestOpenBlock(e.clientY, e.clientX));
-	});
+    var BoardView = React.createClass({
+        render: function() {
+            return (
+                <div className="board">
+                </div>
+            )
+        }
+    });
+    var PaneView = React.createClass({
+        windowBounds: function() {
+            return {
+                width: window.innerWidth,
+                height: window.innerHeight
+            }
+        },
+        updateWindowBounds: function() {
+            this.setState({styles: this.windowBounds()})
+        },
+        getInitialState: function() {
+            var state = {
+                styles: this.windowBounds()
+            }
+            return state;
+        },
+        componentDidMount: function() {
+            window.addEventListener("resize", this.updateWindowBounds);
+        },
+        componentWillUnmount: function() {
+            window.removeEventListener("resize", this.updateWindowBounds);
+        },
+        handleMouseMove: function(e) {
+            console.log("Move", e.clientX, e.clientY);
+        },
+        handleClick: function(e) {
+            console.log("Click", e.clientX, e.clientY);
+        },
+        render: function() {
+            return (
+                <div style={this.state.styles}
+                    onMouseMove={this.handleMouseMove}
+                    onClick={this.handleClick}>
+                    <BoardView />
+                </div>
+            )
+        }
+    })
+    ReactDOM.render(<PaneView />, document.getElementById("pane"))
 
-	// Init
-	setBodySize();
-	board.addBlock(0, 0);
-	board.addBlock(0, 1);
-	board.addBlock(0, 2);
-	board.addBlock(1, 0);
+    // var board = new Board();
+    // board.addBlock(0, 0);
+    // board.addBlock(0, 1);
+    // board.addBlock(0, 2);
+    // board.addBlock(1, 0);
 });
